3e83cb156754f212f6bac8646aaa04d4fa3f4999
Hannah <hannaelsajoy@gmail.com>
2023-11-16 17:37:55 +0530

    DanceAcademy-backend requirements edited

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-$&q@ylxlejkm6=bpb&44060x65rqiy8zegcod10el@)q8-y!wc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
