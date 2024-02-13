064c51323890a086e64c0dbbe0d5c806fbf2de75
Hannah <hannaelsajoy@gmail.com>
2024-02-13 12:19:25 +0530

    cors origin updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -67,8 +67,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:3001",
+    # "http://localhost:3000",
+    # "http://localhost:3001",
 
     "https://dance-academy-rho.vercel.app",
 ]
